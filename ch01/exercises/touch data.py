b25a3b3d2604a47f42debd5c670251ddfe7cf7a4
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-01-30 17:06:57 -0500

    data operations exercise

---
+++ ch01/exercises/touch data.py
@@ -0,0 +1,11 @@
+#predicted results in comments on each line :)
+10 * 5 #50
+10 ** 2 #100
+15 / 10 #1.5
+15 // 10 #1
+-15 // 10 #-1
+15 % 10 #5
+10 % 15 #10
+10 % 10 #0
+0 % 10 #0
+10 / 15 #0.666... it is a repeating decimal, obviously python doesn't print an infinite decimal but it should round up to 0.66....667
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,10 @@
-
+#Part A - broken fix later
+weeks = 16
+classes = 5
+tuition = 6000
+classes_per_week = 3
+cost_per_week = ((tuition / classes) / weeks)
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per week:", cost_per_week)
+print("Cost per class:", cost_per_class)
+#Part B
