32ad72707f91edfc6da01ccffb93f320313b5264
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-01-31 19:11:09 -0500

    did ch 1 lab

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,10 +1,22 @@
-#Part A - broken fix later
+import random
+
+#Part A
 weeks = 16
+print(weeks, type(weeks))
 classes = 5
+print(classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 classes_per_week = 3
+print(classes_per_week, type(classes_per_week))
 cost_per_week = ((tuition / classes) / weeks)
+print(cost_per_week, type(cost_per_week))
 cost_per_class = (cost_per_week / classes_per_week)
+print(cost_per_week, type(cost_per_week))
 print("Cost per week:", cost_per_week)
 print("Cost per class:", cost_per_class)
+
 #Part B
+the_list = ["cool", "fail", 1, 4, 0.0013516167]
+the_pick = random.choice(the_list)
+print("Today's pick is...", the_pick)
