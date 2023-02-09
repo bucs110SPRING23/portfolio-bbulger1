6ab6ede6120ec195ede75f38a6abb40e2807f574
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-02-08 19:01:39 -0500

    for loops

--- ch02/exercises/myturtle.py
+++ ch02/exercises/myturtle.py
@@ -3,18 +3,15 @@ import turtle
 screen = turtle.Screen()
 theturtle = turtle.RawTurtle(screen)
 theturtle.shape("turtle")
-theturtle.color("purple")
 
-for _ in range(4):
-   theturtle.forward(50)
-   theturtle.right(90)
+colors = ["purple", "red", "blue", "magenta", "gray", "cyan", "green", "black", "orange"]
 
-theturtle.color("red")
-theturtle.penup()
-theturtle.forward(100)
-theturtle.left(10)
-
-theturtle.pendown()
-for _ in range(4):
-   theturtle.forward(50)
-   theturtle.left(90)
+for color in colors:
+   theturtle.color(color)
+   for _ in range(4):
+      theturtle.forward(50)
+      theturtle.right(90)
+   theturtle.penup()
+   theturtle.forward(100)
+   theturtle.left(80)
+   theturtle.pendown()
