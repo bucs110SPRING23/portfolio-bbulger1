821dd59f61c4500e824a70001c4d7a98adfa2b7f
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-02-13 13:48:32 -0500

    did lab part a only

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,1 +1,42 @@
-#this had the chapter 1 lab here but not anymore :o
+#PART A
+import random
+import turtle
+
+colorlist = ["red", "purple", "aqua", "orange", "green", "blue", "pink", "gray"]
+shapelist = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+
+screen = turtle.Screen()
+turtle1 = turtle.RawPen(screen)
+turtle2 = turtle.RawTurtle(screen)
+turtles = [turtle1, turtle2]
+for thisturtle in turtles:
+    thisturtle.penup()
+    thisturtle.shape(random.choice(shapelist))
+    thisturtle.color(random.choice(colorlist))
+    thisturtle.speed("slowest")
+
+turtle1.goto(-100, 20)
+turtle2.goto(-100, -20)
+
+#race 1
+
+for thisturtle in turtles:
+    thisturtle.pendown()
+    thisturtle.forward(random.randrange(1,100))
+
+turtle1.goto(-100,20)
+turtle2.goto(-100,-20)
+for thisturtle in turtles:
+    thisturtle.clear()
+    thisturtle.penup()
+
+#race 2
+for thisturtle in turtles:
+    thisturtle.pendown()
+
+for _ in range(10):
+    for thisturtle in turtles:
+        thisturtle.forward(random.randrange(1,10))
+
+turtle1.goto(-100,20)
+turtle2.goto(-100,-20)
