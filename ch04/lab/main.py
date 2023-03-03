00438ff26cee095d325531bb19477bf3e4761fa0
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-03-03 13:12:14 -0500

    working on lab

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -7,6 +7,7 @@ frame = 1
 
 while 1:
     pygame.event.get()
+    
     end = False
     screen = pygame.display.set_mode([1000, 750])
     screensize = pygame.display.get_window_size()
@@ -16,6 +17,9 @@ while 1:
     screencolor = (0, 0, 0, 255)
     targetradius = (screensize[1]/2) * 0.75
     targetpos = [screensize[0]/2, screensize[1]/2 + 1000]
+    gamefont = pygame.font.Font(pygame.font.get_default_font(), 64)
+    gamefont.set_italic(True)
+    istext = False
     
     while 1:
         #Draw
@@ -27,6 +31,10 @@ while 1:
         pygame.draw.circle(screen, "black", targetpos, targetradius, 1)
         pygame.draw.line(screen, "black", [targetpos[0] - targetradius,targetpos[1]], [targetpos[0] + targetradius,targetpos[1]])
         pygame.draw.line(screen, "black", [targetpos[0],targetpos[1] - targetradius], [targetpos[0],targetpos[1] + targetradius])
+        #Text
+        if istext:
+            fontsurface = currentfont.render(message, False, currentfontcolor, currentfontbg)
+            screen.blit(fontsurface, fontcoords)
 
         #Flip
         pygame.display.flip()
@@ -38,8 +46,20 @@ while 1:
                 screencolor = (0, 0, screencolor[2] + 1)
             if targetpos[1] < (screensize[1]/2 + 1): 
                 phase = 1
-        if phase == 1: break
+        if phase == 1:
+            targetpos[1] = screensize[1]/2
+            istext = True
+            currentfont = gamefont
+            currentfontcolor = "white"
+            currentfontbg = False
+            fontcoords = (targetpos[0] - 128, targetpos[1] - 32)
+            message = "DARTS"
+            currentframe = frame
+            if frame == currentframe + 2000:
+                phase = 2
+        if phase == 2: 
+            break
+
         frame += 1
 
     break
-    
