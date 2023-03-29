43ed55e9b9ed1fb1d66c74240e417f8fa5e811b0
bbulger1 <123599299+bbulger1@users.noreply.github.com>
2023-03-29 17:20:37 -0400

    from/import

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -1,17 +1,17 @@
-import rectangle
-import surface
+from rectangle import Rectangle
+from surface import Surface
 
 def main():
-    r = rectangle.Rectangle(10, 10, 10, 10)
+    r = Rectangle(10, 10, 10, 10)
     assert((r.x, r.y, r.height, r.width) == (10, 10, 10, 10))
-    r = rectangle.Rectangle(-1, 1, 1, 1)
+    r = Rectangle(-1, 1, 1, 1)
     assert((r.x, r.y, r.height, r.width) == (-1, 1, 1, 1))
-    r = rectangle.Rectangle(1, -5, 1, 1)
+    r = Rectangle(1, -5, 1, 1)
     assert((r.x, r.y, r.height, r.width) == (1, -5, 1, 1))
-    r = rectangle.Rectangle(1, 1, 1, -1000)
+    r = Rectangle(1, 1, 1, -1000)
     assert((r.x, r.y, r.height, r.width) == (1, 1, 1, -1000))
 
-    s = surface.Surface("myimage.png", 10, 10, 10, 10)
+    s = Surface("myimage.png", 10, 10, 10, 10)
     assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10, 10, 10, 10))
     srect = s.getRect()
     assert((srect.x, s.getRect().y, srect.height, srect.width) == (10, 10, 10, 10))
